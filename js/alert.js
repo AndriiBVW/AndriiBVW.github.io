d6464be7b0da5e2523ca8a07b137d6f3b0ba83a6
Andrew <andriibvw@outlook.com>
2020-08-05 14:33:39 +0300

    add timer for alarm block

--- js/alert.js
+++ js/alert.js
@@ -6,12 +6,28 @@ window.addEventListener('DOMContentLoaded', () => {
   const boxText = document.querySelector('.alarm-box__text');
 
   boxText.textContent = "This and other portfolio pages are demo";
+  
+  if (localStorage.hasOwnProperty('timeAction')) {
+    const newTimeAction = new Date().getTime();
+    const timeAction = JSON.parse(localStorage.getItem('timeAction'));
+    const timer = newTimeAction - timeAction;
+  
+    if (timer > 172800000) {
+      localStorage.clear();
+      showAlarm();
+    }
+  } else {
+    showAlarm();
+  }
 
-  if (!localStorage.hasOwnProperty('alarm')) {
+  function showAlarm() {
     box.style.display = 'block';
     btn.addEventListener('click', () => {
+      
       box.classList.add('alarm-box_closed');
       localStorage.setItem('alarm', true);
+      localStorage.setItem('timeAction', JSON.stringify(new Date().getTime()));
+      
       setTimeout(() => {
         box.style.display = 'none';
       }, 600);
