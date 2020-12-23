ae67f4fbdae597373c9ee5a918427c23ef254d89
Andrew <andriibvw@outlook.com>
2020-12-23 15:31:29 +0200

    test player

--- player/scripts/radioPlayer.js
+++ player/scripts/radioPlayer.js
@@ -4,6 +4,8 @@ export const radioPlayerInit = () => {
   const radioHeaderBig = document.querySelector(".radio-header__big");
   const radioNavigation = document.querySelector(".radio-navigation");
   const radioItem = document.querySelectorAll(".radio-item");
+  const radioVolumeIcon = document.querySelector(".radio-volume_icon");
+  const radioVolumeRange = document.querySelector(".radio-volume_range");
   const radioStop = document.querySelector(".radio-stop");
 
   const audio = new Audio();
@@ -28,10 +30,28 @@ export const radioPlayerInit = () => {
     elem.classList.add("select");
   };
 
+  const changeValue = () => {
+    if (audio.muted) {
+      audio.muted = false;
+    }
+    audio.volume = radioVolumeRange.value / 100;
+  };
+
+  const toggleVolume = () => {
+    audio.muted = !audio.muted;
+    if (audio.muted) {
+      radioVolumeRange.value = 0;
+    } else {
+      radioVolumeRange.value = Math.round(audio.volume * 100);
+    }
+  };
+
   radioNavigation.addEventListener("change", (event) => {
     const target = event.target;
     const parrent = target.closest(".radio-item");
     selectItem(parrent);
+
+    console.log();
 
     const title = parrent.querySelector(".radio-name").textContent;
     radioHeaderBig.textContent = title;
@@ -42,6 +62,7 @@ export const radioPlayerInit = () => {
     radioStop.disabled = false;
 
     audio.src = target.dataset.radioStantion;
+    audio.volume = 0.5;
     audio.play();
     changeIconPlay();
   });
@@ -54,4 +75,8 @@ export const radioPlayerInit = () => {
     }
     changeIconPlay();
   });
+
+  radioVolumeRange.addEventListener("input", changeValue);
+
+  radioVolumeIcon.addEventListener("click", toggleVolume);
 };
